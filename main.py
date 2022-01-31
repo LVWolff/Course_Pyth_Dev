d47bc692bc6be5ff64cc66d79117d48a447ea8f3
lvwolf <lwolff2003@nm.ru>
2022-01-31 20:45:04 +0300

    Test Project Git

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
 import json
 
+def val_info(data, val):
+    print("Валюта " + val + ' курс: ' + str(data["Valute"][val]['Value']))
+
 url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 data = json.loads(response.text)
 
-print(data)
+val_l = data["Valute"].keys() #список валют
+
+while True:
+    val = input('Введите буквенный код валюты (например USD, AUD, KZT и т.д. или Enter для завершения): ').upper()
+    if (not val in val_l) and val != '':
+        continue
+    elif val == '':
+        break
+    else:
+        val_info(data, val)
+
+
